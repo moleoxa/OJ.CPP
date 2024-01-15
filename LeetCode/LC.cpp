ba39257bd2b0cf61a3a9639d04ea6345df79b2c2
QS <qiushang1227@gmail.com>
2024-01-16 01:26:57 +0800

    240115

--- LeetCode/LC.cpp
+++ LeetCode/LC.cpp
@@ -3,6 +3,15 @@
 using namespace std;
 using ll = long long;
 
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
