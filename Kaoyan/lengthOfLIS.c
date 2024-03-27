ad6fbece559708ad73b3dd877e4c4cb77ca7418f
moleoxa <qiushang1227@gmail.com>
2024-03-27 19:42:05 +0800

    230327

---
+++ Kaoyan/lengthOfLIS.c
@@ -0,0 +1,17 @@
+int lengthOfLIS(int* nums, int numsSize) {
+    int pre[numsSize + 1];
+    pre[0] = 10001;
+    int ans = 0;
+    for (int i = 0; i < numsSize; i++) {
+        int x = ans;
+        while (pre[x] >= nums[i]) {
+            x--;
+        }
+        if (x == ans) {
+            pre[++ans] = nums[i];
+        } else if (pre[++x] > nums[i]) {
+            pre[x] = nums[i];
+        }
+    }
+    return ans;
+}
